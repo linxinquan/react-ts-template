6773b62cd4025965261d442e1d36a148bf2e7194
linxinquan <xinquan.lin@163.com>
2025-01-09 09:52:42 +0800

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,18 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
+    'no-continue': 0,
+    'no-param-reassign': [2, { props: false }],
     'import/extensions': 0,
     'react/require-default-props': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/function-component-definition': 0,
+    'react/destructuring-assignment': 0,
+
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
   },
 };
--- declarations.d.ts
+++ declarations.d.ts
@@ -3,3 +3,7 @@ declare module '*.less' {
   const classes: Readonly<Record<string, string>>;
   export default classes;
 }
+declare module '*.glsl' {
+  const str: string;
+  export default str;
+}
